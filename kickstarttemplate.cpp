ebe39ffdeb1ae2e0541198e8c68c8f2181862c39
Yong Hua Tang <tang.yonghua010111@gmail.com>
2020-09-13 22:59:14 +0800

    add

--- kickstarttemplate.cpp
+++ kickstarttemplate.cpp
@@ -1,10 +1,22 @@
 # include <bits/stdc++.h>
 using namespace std;
 
-# define ll long long
-# define ar array
+//# define ll long long
+//# define ar array
+
+void solve() {
+
+}
 
 int main() {
-    cout << "Hello world!";
+    ios::sync_with_stdio(0); //or 'false'
+    cin.tie(0); //or 'NULL'
+
+    int t;
+    cin >> t;
+    for(int i=1; i<=t; ++i) {
+        cout << "Case #" << i << ": ";
+        solve();
+    }
     return 0;
 }
