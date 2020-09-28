9b3e6e6388aa8b4aecd61ea824a474cc4f0e6691
Yong Hua Tang <tang.yonghua010111@gmail.com>
2020-09-28 18:49:18 +0800

    add

---
+++ ks2020rdq1.cpp
@@ -0,0 +1,31 @@
+# include <bits/stdc++.h>
+using namespace std;
+
+int n;
+
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    int t;
+    cin >> t;
+    for (int i=1; i<=t; ++i) {
+        cin >> n;
+        int arr[n];
+        for (int i=0; i<n; ++i) {
+            int num;
+            cin >> num;
+            arr[i]=num;
+        }
+        int max = arr[0];
+        int record_breaking=0;
+        for (int i=0; i<n; ++i) {
+            if (arr[i]>max) {
+                arr[i]=max;
+                ++record_breaking;
+            }
+        }
+        cout << "Case #" << i << ": " << record_breaking;
+    }
+    return 0;
+}
