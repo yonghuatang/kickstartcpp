b14e4920aa748506d149a884e59ceac3fb26480d
James Tang <70836155+yonghuatang@users.noreply.github.com>
2021-12-25 12:06:08 +0800

    Update kickstarttemplate.cpp

--- kickstarttemplate.cpp
+++ kickstarttemplate.cpp
@@ -1,15 +1,15 @@
 # include <bits/stdc++.h>
 using namespace std;
 
+void solve() {
+   
+}
 
 int main() {
-    ios::sync_with_stdio(0);
-    cin.tie(0);
-
     int t;
     cin >> t;
-    for (int tc=1; tc<=t; ++tc) {
-        cout << "Case #" << tc << ": " << "lorem ipsum" << '\n';
+    for (int tc=1; tc<=t; tc++) {
+        cout << "Case #" << tc << ": " << solve() << '\n';
     }
     return 0;
 }
