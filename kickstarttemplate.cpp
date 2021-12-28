cf0886179b45ff4e30ecc62b06c492a283e520b6
Yong Hua Tang <tang.yonghua010111@gmail.com>
2021-12-29 00:53:01 +0800

    update files

--- kickstarttemplate.cpp
+++ kickstarttemplate.cpp
@@ -1,15 +1,12 @@
-# include <bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 
-int solve() {
-    return NULL;
-}
 
 int main() {
     int t;
     cin >> t;
     for (int tc=1; tc<=t; tc++) {
-        cout << "Case #" << tc << ": " << solve() << '\n';
+        cout << "Case #" << tc << ": " << NULL << '\n';
     }
     return 0;
 }
