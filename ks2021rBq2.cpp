02154fa3f840c34d4f0305006f3a13d8b35c0150
Yong Hua Tang <tang.yonghua010111@gmail.com>
2022-01-03 23:13:50 +0800

    ks2021rBq2.cpp

---
+++ ks2021rBq2.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+
+int main() {
+    int t;
+    cin >> t;
+    // DECLARE AND READ VARIABLES HERE
+    for (int tc=1; tc<=t; tc++) {
+        int n;
+        cin >> n;
+        vector<ll> arr;
+        for (auto i: arr) {  // &?
+            cin >> i;
+        }
+        int ans;
+        if (n == 2) {
+            ans = 2;
+        } else {
+
+        }
+        cout << "Case #" << tc << ": " << ans << '\n';
+    }
+    return 0;
+}
