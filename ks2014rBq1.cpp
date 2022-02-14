41da8db8444d1710e6e4e838aea176a6087e1f58
Yong Hua Tang <tang.yonghua010111@gmail.com>
2022-02-14 21:22:47 +0800

    update files

---
+++ ks2014rBq1.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+const int MOD = 1000000007;
+
+int main() {
+    int t;
+    cin >> t;
+    for (int tc=1; tc<=t; tc++) {
+        int m, n, ans = 0;
+        cin >> m >> n;
+
+        cout << "Case #" << tc << ": " << ans << '\n';
+    }
+    return 0;
+}
