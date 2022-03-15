36321b37e3c124f0d66f5ff0498a6e004a2aa059
Yong Hua Tang <tang.yonghua010111@gmail.com>
2022-03-15 14:30:35 +0800

    ks2019rBq1.cpp

--- ks2019rBq1.cpp
+++ ks2019rBq1.cpp
@@ -1,43 +1,3 @@
-// #include <bits/stdc++.h>
-// using namespace std;
-
-// typedef long long ll;
-// const int MOD = 1000000007;
-
-// int main() {
-//     int t;
-//     cin >> t;
-//     for (int tc=1; tc<=t; tc++) {
-//         int n, q, ans = 0;
-//         cin >> n >> q;
-//         string s;
-//         cin >> s;
-//         for (int i=0; i<q; i++) {
-//             int li, ri;
-//             cin >> li >> ri;
-//             string s_temp = s.substr(li-1, ri-li+1);  // (pos, len)
-//             std::sort(s_temp.begin(), s_temp.end());
-//             char currentChar = s_temp[0];
-//             int oddCount = 0, tempCount = 0;
-//             for (int j=0; j<ri-li+1; j++) {
-//                 if (oddCount == 2) {
-//                     break;
-//                 }
-//                 if (s[j] == currentChar) {
-//                     tempCount++;
-//                 } else {
-//                     currentChar = s[j];
-//                     oddCount += (tempCount % 2 != 0);
-//                     tempCount = 1;
-//                 }
-//             }
-//             ans += (oddCount < 2);
-//         }
-//         cout << "Case #" << tc << ": " << ans << '\n';
-//     }
-//     return 0;
-// }
-
 #include <bits/stdc++.h>
 using namespace std;
 
@@ -52,26 +12,21 @@ int main() {
         cin >> n >> q;
         string s;
         cin >> s;
+        vector<vector<int>> v(n+1, vector<int>(26, 0)); // prefix sum
+        for (int i=0; i<n; i++) {
+            v[i+1] = v[i];
+            v[i+1][s[i]-65]++;
+        }
         for (int i=0; i<q; i++) {
-            int li, ri;
+            int li, ri;  // inclusive
             cin >> li >> ri;
-            if (li == ri) {
-                ans++;
-                continue;
-            }
-            string s_temp = s.substr(li-1, ri-li+1);  // (pos, len)
-            std::sort(s_temp.begin(), s_temp.end());
-            vector<int> dp(26, 0);
-            for (const char& c : s_temp) {
-                dp[(int)c-65]++;
-            }
             int oddCount = 0;
-            for (auto it=dp.begin(); it!=dp.end() && oddCount!=2; it++) {
-                oddCount += (*it % 2 != 0);
+            for (int j=0; j<26; j++) {
+                oddCount += (v[ri][j] - v[li-1][j]) % 2 != 0;
             }
-            ans += (oddCount < 2);
+            ans += oddCount <= 1;
         }
         cout << "Case #" << tc << ": " << ans << '\n';
     }
     return 0;
-}  // TLE
+}
